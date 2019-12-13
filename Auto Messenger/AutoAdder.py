1338843dbf3b97ff0a92732426d9785dea52dbb6
Jack Hales <halesyy@gmail.com>
2019-12-13 13:40:55 +1100

    Frame and necessary functions built for the creation of AM, just getting a test phone

--- Auto Messenger/AutoAdder.py
+++ Auto Messenger/AutoAdder.py
@@ -4,37 +4,37 @@
 # Or Facebook:        https://facebook.com/jek.hales
 
 import pyautogui as pag
-import sys, json, requests, keyboard, time
+import sys, json, requests, keyboard, time, pyperclip
 
 reqNames = requests.get("https://snaphame.herokuapp.com/names")
 names = reqNames.json()
 alreadyUsed = ['one']
 
-print("Got first names: {0}".format(names))
-print("Waiting for you to press 'enter' (for fame!)")
+# print("Got first names: {0}".format(names))
 
+print("Leave the below field empty if you want us to pick a proven message...\n")
+yourMessage = input("What do you want to paste into each chat?: ")
+
+print("\nWaiting for you to press 'enter' (for fame!)")
 while True:
     if keyboard.read_key() == "enter":
         print("Starting mover")
         break
 
-def addName(name):
-    # recording where the mouse first started at
-    startedAt = pag.position()
-    pag.typewrite(name, interval=0.10)
-    time.sleep(1)
-    moveDown(70)
-    # moves down 6 times, down 30 pixels (adjust for bigger/smaller movements)
-    for i in range(6):
-        pag.click()
-        moveDown(22)
-    pag.moveTo(startedAt[0], startedAt[1], 1)
+# Action:
+# 0. Ask user if they wish to use a message, or if not just give
+#    them a random message?
+# 1. Wait for enter, sitting over someone's username
+# 2. Click (going to open the profile)
+# 3. Paste message (from api, or local random array)
+#    Hit enter
+# 4. Swipe back (click, drag out - OR - have a point of interest where the user goes back - IF CASE - move back and re-structure starting point)
+# 5. Drag down X pix - OR - move down pix amount, that limits though..
+# 6. Repeat this process
 
-    doRefresh()
-    pag.moveTo(startedAt[0], startedAt[1], 1)
-    moveLeft(50, 0.1)
-    pag.click()
-    moveRight(50, 0)
+# Dragging is possible! Use the function `pyautogui.dragTo(100, 200, button='left')` as a guide for this
+# Great, this will have to be adapted into a more abstract function to get in and out of areas
+# such as the chat.
 
 # a few functions I wrote to make it easier to move around, since
 # pyautogui new versions don't support the move function alone
@@ -54,6 +54,24 @@ def moveLeft(px, time=0.5):
     now = pag.position()
     pag.moveTo(now[0]-px, now[1], time)
 
+def dragLeft(px, time=0.5):
+    now = pag.position()
+    pag.dragTo(now[0]-px, now[1], time, button='left')
+
+def dragUp(px, time=0.5):
+    now = pag.position()
+    pag.dragTo(now[0], now[1]-px, time, button='left')
+
+pyperclip.copy("This is a test if this works...")
+
+# pag.click()
+pag.hotkey('ctrl', 'v')
+# pag.press('enter')
+# dragLeft(100, time=0.3)
+# dragUp(50, time=0.2)
+
+
+
 # a short-cut to refreshing from the start position on certain
 # screen size
 def doRefresh():
@@ -69,10 +87,10 @@ def getNewNames():
     # names = reqNames.json()
     return reqNames.json()
 
-for i in range(3):
-    time.sleep(5)
-    for name in names["names"]:
-        if name in alreadyUsed: continue
-        addName(name)
-        alreadyUsed.append(name)
-    names = getNewNames()
+# for i in range(3):
+#     time.sleep(5)
+#     for name in names["names"]:
+#         if name in alreadyUsed: continue
+#         addName(name)
+#         alreadyUsed.append(name)
+#     names = getNewNames()
